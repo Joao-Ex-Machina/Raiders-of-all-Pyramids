57219c7b50a65adabfa0a72a152e1b9b593c3137
João Rodrigues <80014156+Joao-Ex-Machina@users.noreply.github.com>
2021-10-07 22:19:53 +0000

    Add files via upload

--- src/alphaopsmodule.c
+++ src/alphaopsmodule.c
@@ -10,7 +10,8 @@
 #include <roap.h>
 
 // START 05 Out Im going to sleep
-// 
+// revised on 07 oct
+//
 int variant_test(int** matrix, int targetcellline, int targetcellcol, int targetcellline2, int targetcellcol2, char varID[2], int lines, int colummns){
 	targetcellline -=1; //update to target to be on pair with a matrix starting in (0,0)
 	targetcellcol -=1;
@@ -29,22 +30,26 @@ int variant_test(int** matrix, int targetcellline, int targetcellcol, int target
 			}
 		break;
 		case '2':
-			result=check_neighbours(matrix, targetcellline, targetcellcol, lines, colummns, white)
+			result=check_neighbours(matrix, targetcellline, targetcellcol, lines, colummns, white);
 		break;
 		case '3':
-			result=check_neighbours(matrix, targetcellline, targetcellcol, lines, colummns, grey)
+			result=check_neighbours(matrix, targetcellline, targetcellcol, lines, colummns, grey);
 		break;
 		case '4':
-			result=check_neighbours(matrix, targetcellline, targetcellcol, lines, colummns, black)
+			result=check_neighbours(matrix, targetcellline, targetcellcol, lines, colummns, black);
 		break;
 		case '5':
-			result=check_breakability
+			result=check_breakability;
 		break;
 		case '6':
+			//TBI
+		break;
 	}
 	return result;
-}
+}int** matrix, int targetcelllline, int targetcellcol, int lines, int columns
 
+//started on 06 oct
+//revised 07 oct
 int check_neighbours (int** matrix, int targetcelllline, int targetcellcol, int lines, int columns, int colour){
 	int bounds[4]={0,0,0,0};
 	if(targetcellline > (lines-1) || targetcellline < 0 || targetcellcol > (colummns-1) || targetcellcol < 0){
@@ -92,17 +97,37 @@ int check_neighbours (int** matrix, int targetcelllline, int targetcellcol, int 
 	}
 
 }
-
+ //07 oct
 int check_breakability(int** matrix, int targetcelllline, int targetcellcol, int lines, int columns){
 	int bounds[4]={0,0,0,0};
+	int result=0;
         if(targetcellline > (lines-1) || targetcellline < 0 || targetcellcol > (colummns-1) || targetcellcol < 0){
                 result=-2;
                 return result;
         }
 
         bounds=check_bounds(targetcellline, targetcellcol,lines, colummns);
-}
+	if(matrix[targetcellline][targetcellcol]<=white){ //check if cell is grey
+		return result;
+	}
 
+	if(bounds[left]==0 && bounds[right]==0){ //Verify if there's any bound. In positive case discard that path
+		if(matrix[targetcellline][targetcellcol-1]==0 && matrix[targetcellline][targetcellcol+1]==0){
+			result=1;
+			return result;
+		}
+
+	}
+	if(bounds[up]==0 && bounds[down]==0){
+                if(matrix[targetcellline-1][targetcellcol]==0 && matrix[targetcellline+1][targetcellcol]==0){
+                        result=1;
+                        return result;
+                }
+
+        }
+	return result;
+}
+//07 oct
 int* check_bounds(int targetcellline, int targetcellcol, int lines, int colummns){
 	int bounds[4]={0,0,0,0};//left,up,down,right
 	if(targetcellcol==0)
--- src/readfile.c
+++ src/readfile.c
@@ -14,17 +14,18 @@
   Input: Pointer to char (name of the file to be read)
   Output: pointer to pointer to int (matrix)
   Date Created: 05 Oct 2021
-  Last Revised:
+  Last Revised: 07 Oct 2021
   Definition:
   Based on: 
 */
  int** readfile(char*_filenamein){
 	int** matrix = NULL;
-	int readctrl = -1, readcnt = -0;
+	int readctrl = -1, readcnt = -0, result=0;
 	int lines=0, colummns=0, cellline=0, cellcol=0, celldata=0, targetcellline=0, targetcelllcol=0,targetcellline2=0, targetcelllcol2=0;
 	char varID[2] ={\0};
 	char* datastr = NULL;
 	FILE* fp = fopen(_filenamein,"r");
+	FILE* fpout=fopen(_filenameout, "w");
 	if (fp == NULL)
 		help(Read_Error,File_Not_Found); //TBI
 	while (feof(fp)==0){
@@ -35,14 +36,18 @@
 				fscanf(fp, "%d %d", &targetcellline2, &targetcelllcol2,);
 			fscanf(fp, "%d", &readctrl)
 			matrix = matrixalloc(lines, colummns); //generate read matrix
-			while (readcnt<=reactrl){
+			while (readcnt<reactrl){
 				fscanf(fp, "%d %d %d", &cellline, &cellcol, &celldata);
 				cellseed(matrix, cellline, cellcol, celldata);
 				readcnt++;
 			}
-			variant_test(matrix, targetcellline, targetcelllcol, targetcellline2, targetcelllcol2); //TBF
-                	freematrix(matrix, lines, colummns);
+			result=variant_test(matrix, targetcellline, targetcelllcol, targetcellline2, targetcelllcol2); //TBF
+                	fprintf(fpout,"%d",result); //TBI
+			fprintf(fpout,"\n");
+			freematrix(matrix, lines, colummns);
 		
 		}
+	fclose(fp);
+	fclose(fpout);//TBI
 
  }
